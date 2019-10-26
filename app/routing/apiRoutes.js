114167c3e6db59dad7db29f766097738ded2676f
Sean Lim <lim_s@outlook.com>
2019-10-26 04:18:32 -0500

    friend pairing logic!

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,13 +12,6 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     friendsData.push(req.body);
-    console.log("post!");
     res.json(friendsData);
-  });
-
-  app.post("/api/clear", function(req, res) {
-    friendsData.length = 0;
-
-    res.json({ ok: true });
   });
 };
